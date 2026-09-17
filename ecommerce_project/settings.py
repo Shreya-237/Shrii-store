f2996ecca3f6947c3c775590196209ee04b38f15
Shreya Singh <singhshreya56075@gmail.com>
2026-09-17 22:24:15 +0530

    Fix allowed hosts

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-gsw1w=qct28b0x9fi077y2_m+r+d=3zz0liv_gk851+@@qqrxd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["shrii-store.onrender.com"]
 
 
 # Application definition
